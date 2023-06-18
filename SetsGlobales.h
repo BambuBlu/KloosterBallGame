492892b58d0360bb5c51ab45fa690baf923b051e
FanncuS4n <118911524+FanncuS4n@users.noreply.github.com>
2023-06-18 01:26:04 -0300

    A

--- SetsGlobales.h
+++ SetsGlobales.h
@@ -6,15 +6,12 @@
 */
 
 #define max_main_menu 4
-
 #define PIXELTOMETER 20
 #define PADDLE_SPEED (-15)
 #define PADDLE_MAX_ANGLE 0.6
 #define PADDLE_MIN_ANGLE (-0.4) 
 #define MAX_VELOCITY_X 30
 #define MAX_VELOCITY_Y 30
-
 #define MIN_VELOCITY_Y 3
-
 
 #endif 
